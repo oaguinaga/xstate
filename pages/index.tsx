8cd03b4883456e8ee862bdf925a2e0a641308938
ogaguinaga <115434003+ogaguinaga@users.noreply.github.com>
2023-10-13 10:40:30 +0200

    my first machine

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,26 @@
+import { useMachine } from "@xstate/react";
+import { myMachine } from "../machine/myFirstMachine";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  const [state, send] = useMachine(myMachine);
+
+  return (
+    <div>
+      {JSON.stringify(state.value)}
+      <button
+        onClick={() => {
+          send("MOUSEOVER");
+        }}
+      >
+        Mouse over
+      </button>
+      <button
+        onClick={() => {
+          send("MOUSEOUT");
+        }}
+      >
+        Mouse out
+      </button>
+    </div>
+  );
 }
